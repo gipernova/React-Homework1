f5f7288fb48cf559349acd709a552c4cd7e09f40
YerominVova <yeriominvova@gmail.com>
2020-10-23 22:23:59 +0300

    Updates

---
+++ precache-manifest.e2914262f3b45011d7b5ed949e3b21d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adeefb0990b1100bf232ecf01df632ab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "129358021bb2d99f71af",
+    "url": "./static/css/main.daa9d829.chunk.css"
+  },
+  {
+    "revision": "e4f6494ba5e1d1ec32d5",
+    "url": "./static/js/2.8ef34f73.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "./static/js/2.8ef34f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "129358021bb2d99f71af",
+    "url": "./static/js/main.1b85d53e.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  },
+  {
+    "revision": "e45d9d1f63b5b15b9ebf933e7d9088d3",
+    "url": "./static/media/252.e45d9d1f.png"
+  },
+  {
+    "revision": "e45ebd5258d77392cb78ceb7b3133eeb",
+    "url": "./static/media/gitpicpng.e45ebd52.png"
+  }
+]);
